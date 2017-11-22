4f87e8e4e5b67cfcc7c2c10b244524a338fa823f
Gerd Tautenhahn <gerd.tautenhahn@saxess-software.de>
2017-11-22 12:55:27 +0100

    Update Create_a_Database.sql

--- 1c. Working with Azure/Create_a_Database.sql
+++ 1c. Working with Azure/Create_a_Database.sql
@@ -1,11 +1,11 @@
 /*
  This script creates a new Azure Database, optional within a pool
  After that you must
-	- Execute the Create Script in DE or EN for a Cloud Cluster (this will also create the User FactoryService in this Database)
 	- Create the user for this Cluster in this master database if they don't exist there already
 	- Create the user for this Cluster in this database
 */
 
-CREATE DATABASE AWT_Niendorf
+CREATE DATABASE DE68
 	-- optional, within a database pool
-	( SERVICE_OBJECTIVE = ELASTIC_POOL ( name = [DataFactory01-ElasticDatabasePool] ) ) 
+	( SERVICE_OBJECTIVE = ELASTIC_POOL ( name = [DataFactory01-EDP01] ) ) 
+GO
