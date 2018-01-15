adcf2703dd05596483388744681c6ab856bb4ecc
MandyHauck <mandy.hauck@saxess-software.de>
2018-01-15 13:49:38 +0100

    Added Grant Execute

--- 3c. DataFactory - Data Input and Database Configuration/Import Structure and Data/Import from Loadtables/import.sp_POST_fValuesPDTV.sql
+++ 3c. DataFactory - Data Input and Database Configuration/Import Structure and Data/Import from Loadtables/import.sp_POST_fValuesPDTV.sql
@@ -1,11 +1,9 @@
 
 /*
-
 Stefan Lindenlaub
 2018/01
-
 Import Data from load.tfValues using PostProductDataTableValues
-
+	EXEC [import].[sp_POST_fValuesPDTV] '','','','',0
 */
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[import].[sp_POST_fValuesPDTV]') AND type in (N'P', N'PC'))
@@ -13,52 +11,50 @@ DROP PROCEDURE [import].[sp_POST_fValuesPDTV]
 GO
 
 CREATE PROCEDURE [import].[sp_POST_fValuesPDTV]
-				(	@FactoryID AS NVARCHAR(255) = '',
-					@ProductlineID AS NVARCHAR(255)	= '',
-					@ProductID AS NVARCHAR(255)	= '',
-					@ValueSeriesID AS NVARCHAR (255) 	= '',
+				(	@FactoryID 	AS NVARCHAR(255) = '',
+					@ProductlineID 	AS NVARCHAR(255) = '',
+					@ProductID	AS NVARCHAR(255) = '',
+					@ValueSeriesID 	AS NVARCHAR (255) = '',
 					@TimeID		 AS INT = 0)	   
 
 AS
-
 SET NOCOUNT ON
 
-DECLARE @TimestampCall AS DATETIME = CURRENT_TIMESTAMP
-DECLARE @ProcedureName AS NVARCHAR (255) = OBJECT_NAME(@@PROCID)
-DECLARE @RC INT
+-------------------------------------------------------------------------------------------------------------------
+-- ##### VARIABLES ###########
+DECLARE @TimestampCall 				AS DATETIME = CURRENT_TIMESTAMP
+DECLARE @ProcedureName 				AS NVARCHAR (255) = OBJECT_NAME(@@PROCID)
+DECLARE @RC 					AS INT
 
-DECLARE @ValueFormula			 AS NVARCHAR (255)
-DECLARE @ValueINT				 AS INT
-DECLARE @ValueText				 AS NVARCHAR (255)
-DECLARE @ValueComment			 AS NVARCHAR (255)
+DECLARE @ValueFormula			 	AS NVARCHAR (255)
+DECLARE @ValueINT				AS INT
+DECLARE @ValueText				AS NVARCHAR (MAX)
+DECLARE @ValueComment			 	AS NVARCHAR (255)
 
 DECLARE @IsIncrementalValuesFlag		AS INT
-DECLARE	@ValuesInBracketsCommaSeparated AS NVARCHAR(MAX) 
+DECLARE	@ValuesInBracketsCommaSeparated 	AS NVARCHAR(MAX) 
 
 DECLARE @ToDoList AS TABLE
-			(     
-				   ProductID NVARCHAR (255)
-				  ,ProductLineID NVARCHAR (255)
-				  ,FactoryID NVARCHAR (255)
-				  ,IsIncrementalValuesFlag INT
-				  ,ValuesInBracketsCommaSeparated NVARCHAR(MAX)
-
-			)
-
+	(	 ProductID 				NVARCHAR (255)
+		,ProductLineID 				NVARCHAR (255)
+		,FactoryID 				NVARCHAR (255)
+		,IsIncrementalValuesFlag 		INT
+		,ValuesInBracketsCommaSeparated 	NVARCHAR(MAX))
+				
+-------------------------------------------------------------------------------------------------------------------
+-- ##### TEMPORARY TABLES ###########
 -- Temporäre Tabelle als Datensammler
 IF OBJECT_ID('tempdb..#DATA') IS NOT NULL DROP TABLE #DATA
 CREATE TABLE #DATA	
-				(
-				 ProductID		NVARCHAR (255)
-				,ProductLineID	NVARCHAR (255)
-				,FactoryID		NVARCHAR (255)
-				,ValueSeriesID	NVARCHAR (255)
-				,TimeID			INT
-				,ValueFormula	NVARCHAR (255)
-				,ValueINT		BIGINT
-				,ValueText		NVARCHAR (255)
-				,ValueComment	NVARCHAR (255)	
-				)
+		(ProductID		NVARCHAR (255)
+		,ProductLineID		NVARCHAR (255)
+		,FactoryID		NVARCHAR (255)
+		,ValueSeriesID		NVARCHAR (255)
+		,TimeID			INT
+		,ValueFormula		NVARCHAR (255)
+		,ValueINT		BIGINT
+		,ValueText		NVARCHAR (MAX)
+		,ValueComment		NVARCHAR (2555))
 
 INSERT INTO #DATA
 		SELECT   tfV.ProductID		
@@ -77,7 +73,7 @@ INSERT INTO #DATA
 		  AND	(@ValueSeriesID = '' OR tfV.ValueSeriesID = @ValueSeriesID)
 		  AND   (@TimeID = 0 OR tfV.TimeID = @TimeID)
 
-
+-- Tabelle für String
 INSERT INTO @ToDoList
 	SELECT	 d.ProductID
 			,d.ProductLineId
@@ -96,13 +92,15 @@ INSERT INTO @ToDoList
 	FROM #DATA d
 	GROUP BY   d.ProductID, d.ProductLineId, d.FactoryID
 
+-------------------------------------------------------------------------------------------------------------------
+-- ##### POST ###########
 DECLARE MyCursor CURSOR STATIC FOR
 
-	SELECT tdl.ProductID
-		  ,tdl.ProductLineID
-		  ,tdl.FactoryID
-		  ,tdl.IsIncrementalValuesFlag
-		  ,''+LEFT(tdl.ValuesInBracketsCommaSeparated,LEN(tdl.ValuesInBracketsCommaSeparated)-1)+''
+	SELECT 	 tdl.ProductID
+		,tdl.ProductLineID
+		,tdl.FactoryID
+		,tdl.IsIncrementalValuesFlag
+		,''+LEFT(tdl.ValuesInBracketsCommaSeparated,LEN(tdl.ValuesInBracketsCommaSeparated)-1)+''
 	FROM @ToDoList tdl
 
 OPEN MyCursor
@@ -121,3 +119,9 @@ FETCH MyCursor INTO @ProductID,@ProductLineID,@FactoryID, @IsIncrementalValuesFl
 END
 CLOSE MyCursor
 DEALLOCATE MyCursor
+
+-------------------------------------------------------------------------------------------------------------------
+GO
+GRANT EXECUTE ON OBJECT ::[import].[sp_POST_fValuesPDTV] TO pf_PlanningFactoryUser;
+GRANT EXECUTE ON OBJECT ::[import].[sp_POST_fValuesPDTV] TO pf_PlanningFactoryService;
+GO
