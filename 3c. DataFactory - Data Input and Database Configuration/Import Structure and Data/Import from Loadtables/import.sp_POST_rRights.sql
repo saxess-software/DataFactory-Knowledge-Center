671c29d9771f4eb2fe8c0c92474e39021b5630a3
MandyHauck <mandy.hauck@saxess-software.de>
2018-01-15 14:26:55 +0100

    Added GRANT EXEC and formatting

--- 3c. DataFactory - Data Input and Database Configuration/Import Structure and Data/Import from Loadtables/import.sp_POST_rRights.sql
+++ 3c. DataFactory - Data Input and Database Configuration/Import Structure and Data/Import from Loadtables/import.sp_POST_rRights.sql
@@ -1,11 +1,8 @@
-
 /*
-
 Stefan Lindenlaub
 2018/01
-
 Import Rights from load.trRights
-
+	EXEC [import].[sp_POST_rRights] '',''
 */
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[import].[sp_POST_rRights]') AND type in (N'P', N'PC'))
@@ -13,17 +10,18 @@ DROP PROCEDURE [import].[sp_POST_rRights]
 GO
 
 CREATE PROCEDURE [import].[sp_POST_rRights]
-			      @UserName AS NVARCHAR(255),
-				  @Right AS NVARCHAR(255)
+			   (	@UserName AS NVARCHAR(255),
+					@Right AS NVARCHAR(255))
 
 AS
-
 SET NOCOUNT ON
 
-DECLARE @TimestampCall AS DATETIME = CURRENT_TIMESTAMP
-DECLARE @ProcedureName AS NVARCHAR (255) = OBJECT_NAME(@@PROCID)
-DECLARE @RC INT
-DECLARE @EffectedRows AS INT = 0
+-------------------------------------------------------------------------------------------------------------------
+-- ##### VARIABLES ###########
+DECLARE @TimestampCall			AS DATETIME = CURRENT_TIMESTAMP
+DECLARE @ProcedureName			AS NVARCHAR (255) = OBJECT_NAME(@@PROCID)
+DECLARE @RC						AS INT
+DECLARE @EffectedRows			AS INT = 0
 
 DECLARE @FactoryID				AS NVARCHAR(255)
 DECLARE @ProductLineID			AS NVARCHAR(255)
@@ -31,17 +29,17 @@ DECLARE @ProductID				AS NVARCHAR(255)
 DECLARE	@ReadCommentMandatory   AS INT
 DECLARE @WriteCommentMandatory  AS INT
 
--- Temptable for Cursor
-
+-------------------------------------------------------------------------------------------------------------------
+-- ##### TEMPORARY TABLES ###########
 IF OBJECT_ID('tempdb..#tmp') IS NOT NULL DROP TABLE #tmp
-	CREATE TABLE #tmp (		 UserName		 NVARCHAR(255)
-							,FactoryID		 NVARCHAR(255)
-							,ProductLineID   NVARCHAR(255)
-							,ProductID		 NVARCHAR(255)
-							,[Right]		 NVARCHAR(255)
-							,ReadCommentMandatory INT
-							,WriteCommentMandatory INT
-						    )
+CREATE TABLE #tmp 
+	(		 UserName				NVARCHAR(255)
+			,FactoryID				NVARCHAR(255)
+			,ProductLineID			NVARCHAR(255)
+			,ProductID				NVARCHAR(255)
+			,[Right]				NVARCHAR(255)
+			,ReadCommentMandatory	INT
+			,WriteCommentMandatory	INT)
 
 INSERT INTO #tmp
 	SELECT   trR.UserName		 
@@ -55,27 +53,31 @@ INSERT INTO #tmp
 	WHERE	(@UserName = '' OR trR.UserName = @UserName)
 	  AND   (@Right = '' OR trR.[Right] = @Right)
 
-
+-------------------------------------------------------------------------------------------------------------------
+-- ##### POST ###########
 DECLARE MyCursor CURSOR FOR
-	SELECT *
-	FROM #tmp t
+	SELECT * FROM #tmp t
 OPEN MyCursor
 FETCH MyCursor INTO @UserName, @FactoryID, @ProductLineID, @ProductID, @Right, @ReadCommentMandatory, @WriteCommentMandatory
 WHILE @@FETCH_STATUS = 0
 BEGIN
-EXECUTE @RC = [dbo].[sx_pf_POST_Right]
-		'SQL'
-		,@UserName				
-		,@FactoryID				
-		,@ProductLineID						
-		,@Right					
-		,@ReadCommentMandatory 
-		,@WriteCommentMandatory
+	EXECUTE @RC = [dbo].[sx_pf_POST_Right]
+			'SQL'
+			,@UserName				
+			,@FactoryID				
+			,@ProductLineID						
+			,@Right					
+			,@ReadCommentMandatory 
+			,@WriteCommentMandatory
 
-Print @RC
+	Print @RC
 FETCH MyCursor INTO @UserName, @FactoryID, @ProductLineID, @ProductID, @Right, @ReadCommentMandatory, @WriteCommentMandatory
 END
 CLOSE MyCursor
 DEALLOCATE MyCursor
 
+-------------------------------------------------------------------------------------------------------------------
+GO
+GRANT EXECUTE ON OBJECT ::[import].[sp_POST_rRights] TO pf_PlanningFactoryUser;
+GRANT EXECUTE ON OBJECT ::[import].[sp_POST_rRights] TO pf_PlanningFactoryService;
 GO
