4401098aae48cbfffdeec4605c469385a42af8bf
Mandy Hauck <mandy.hauck@saxess-software.de>
2018-10-25 10:14:16 +0200

    Update project folder

--- 8. Harmonising standards/01_Project organisation/02_Project Folder/02_Powershell/Create_ProjectFolderStructure.ps1
+++ 8. Harmonising standards/01_Project organisation/02_Project Folder/02_Powershell/Create_ProjectFolderStructure.ps1
@@ -13,65 +13,64 @@
 # Project folder sample
 New-Item -ItemType directory -Name '01_Projektordner Muster'
 
-# Folder for documentation and customer files
-New-Item -ItemType directory -Name '01_Projektordner Muster\01_Projektunterlagen'
-New-Item -ItemType directory -Name '01_Projektordner Muster\01_Projektunterlagen\01_Projektdokumentation'
-New-Item -ItemType File -Name '01_Projektordner Muster\01_Projektunterlagen\01_Projektdokumentation\dummy.txt'
-
-New-Item -ItemType directory -Name '01_Projektordner Muster\01_Projektunterlagen\02_Kundenunterlagen'
-New-Item -ItemType File -Name '01_Projektordner Muster\01_Projektunterlagen\02_Kundenunterlagen\dummy.txt'
-
-# Folder development
-## Folder for all SQL files
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung'
-
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\01_Staging'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\01_Staging\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\02_Param'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\02_Param\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\03_Calc'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\03_Calc\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\04_Control'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\04_Control\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\05_Result'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\01_Permanente Erweiterungstabellen\05_Result\dummy.sql'
-
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\01_Header, CleanUp'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\01_Header, CleanUp\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\02_StandardAPI'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\02_StandardAPI\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\03_API-Modifikationen'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\03_API-Modifikationen\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\04_Staging'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\04_Staging\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\05_Load'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\05_Load\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\06_Param'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\06_Param\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\07_Calc'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\07_Calc\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\08_Control'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\08_Control\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\09_Result'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\02_ProjektAPI\09_Result\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\03_Templates und Struktur'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\03_Templates und Struktur\dummy.sql'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\04_Kleine Helfer'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\01_SQL-Dateien\04_Kleine Helfer\dummy.sql'
-
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\02_XLS-Dateien'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\02_XLS-Dateien\01_ReportingMappen'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\02_XLS-Dateien\01_ReportingMappen\dummy.xls'
-
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\03_PFE-Dateien'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\03_PFE-Dateien\01_Templates,Parameter,Mapping'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\03_PFE-Dateien\01_Templates,Parameter,Mapping\dummy.pfe'
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\03_PFE-Dateien\02_Factories,ProductLines,Products'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\03_PFE-Dateien\02_Factories,ProductLines,Products\dummy.pfe'
+# Folder for customer files
+New-Item -ItemType directory -Name '01_Projektordner Muster\01_Kundenunterlagen'
+New-Item -ItemType directory -Name '01_Projektordner Muster\01_Kundenunterlagen\01_Archiv'
+New-Item -ItemType File -Name '01_Projektordner Muster\01_Kundenunterlagen\01_Archiv\dummy.txt'
 
 
-New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektentwicklung\04_PS-Dateien'
-New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektentwicklung\04_PS-Dateien\dummy.ps1'
+# Folder implementation
+## Folder for all files related to project API
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung'
+
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\01_Staging'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\01_Staging\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\02_Param'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\02_Param\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\03_Calc'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\03_Calc\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\04_Control'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\04_Control\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\05_Result'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\01_Permanente Erweiterungstabellen\05_Result\dummy.sql'
+
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\01_Header, CleanUp'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\01_Header, CleanUp\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\02_StandardAPI'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\02_StandardAPI\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\03_API-Modifikationen'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\03_API-Modifikationen\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\04_Staging'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\04_Staging\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\05_Load'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\05_Load\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\06_Param'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\06_Param\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\07_Calc'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\07_Calc\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\08_Control'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\08_Control\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\09_Result'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\02_ProjektAPI\09_Result\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\03_Templates und Struktur'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\03_Templates und Struktur\dummy.sql'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\04_Kleine Helfer'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\01_ProjektAPI\04_Kleine Helfer\dummy.sql'
+
+## Folder for all files related to reporting
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\02_Reporting'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\02_Reporting\01_Auslieferung'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\02_Reporting\01_Auslieferung\dummy.xls'
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\02_Reporting\02_Monitoring'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\02_Reporting\02_Monitoring\dummy.xls'
+
+## Folder for all templates
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\03_Templates'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\03_Templates\dummy.pfe'
+
+## Folder for all automation related topics
+New-Item -ItemType directory -Name '01_Projektordner Muster\02_Projektumsetzung\04_Automatisierung'
+New-Item -ItemType File -Name '01_Projektordner Muster\02_Projektumsetzung\04_Automatisierung\dummy.ps1'
