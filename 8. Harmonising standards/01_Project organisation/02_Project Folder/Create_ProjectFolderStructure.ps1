68e8bb0b2f6ad4c11c006fc937d6a13755541744
Mandy Hauck <mandy.hauck@saxess-software.de>
2018-11-08 15:32:56 +0100

    Rename Projektmappe

--- 8. Harmonising standards/01_Project organisation/02_Project Folder/Create_ProjectFolderStructure.ps1
+++ 8. Harmonising standards/01_Project organisation/02_Project Folder/Create_ProjectFolderStructure.ps1
@@ -22,7 +22,7 @@ If (Test-Path $folder) { Remove-Item $folder -Recurse; }
 # Project folder sample
 New-Item -ItemType directory -Name '01_Projektordner Muster'
 
-Copy-Item "..\\03_Project Documentation\Template Project Documentation.xlsx" -Destination "01_Projektordner Muster\Projektdokumentation.xlsx"
+Copy-Item "..\\03_Project Documentation\ProjektMappe.xlsx" -Destination "01_Projektordner Muster\ProjektMappe.xlsx"
 
 
 # Folder for customer files
