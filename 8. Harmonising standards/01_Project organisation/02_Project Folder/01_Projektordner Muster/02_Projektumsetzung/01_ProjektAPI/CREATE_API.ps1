8819a81045e51bef1d4f4b6bc562173b7529d66e
MariaRuedger <maria.ruedger@saxess-software.de>
2018-12-14 15:04:31 +0100

    separate Dateien für API und Tabellen, Header vor Tabellen

--- 8. Harmonising standards/01_Project organisation/02_Project Folder/01_Projektordner Muster/02_Projektumsetzung/01_ProjektAPI/CREATE_API.ps1
+++ 8. Harmonising standards/01_Project organisation/02_Project Folder/01_Projektordner Muster/02_Projektumsetzung/01_ProjektAPI/CREATE_API.ps1
@@ -1,4 +1,7 @@
-﻿# Creates API for database(s)
+﻿## VERSION 1.3 ##
+
+
+# Creates API for database(s)
 # The script can have empty Folders
 
     #Object for Encoding
@@ -30,5 +33,33 @@
     [System.IO.File]::WriteAllLines($path, (Get-Content $path),$Utf8NoBomEncoding)
 
 
+# Creates custom tables for database(s)
+# The script can have empty Folders
 
+    #Object for Encoding
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+
+    #File Name
+    $strScriptName = "Tabellen_DataFactory.sql"
+
+    #Deletion old file and creation new file
+    If (Test-Path $strScriptName) {
+                                    Remove-Item $strScriptName
+                                   }
+
+								   
+         #Permanente Erweiterungstabellen
+    cat '.\02_CustomAPI\01_Header, CleanUp\01_Header for API script.sql',
+		'.\01_Permanente Erweiterungstabellen\01_Staging\*.sql',
+        '.\01_Permanente Erweiterungstabellen\02_Param\*.sql',
+        '.\01_Permanente Erweiterungstabellen\03_Calc\*.sql',
+        '.\01_Permanente Erweiterungstabellen\04_Control\*.sql',
+        '.\01_Permanente Erweiterungstabellen\05_Result\*.sql'        > $strScriptName -Encoding "UTF8"
+
+		
+    #Really UTF-8 without BOM
+    $path = $strScriptName
+    [System.IO.File]::WriteAllLines($path, (Get-Content $path),$Utf8NoBomEncoding)
+
+	
 
