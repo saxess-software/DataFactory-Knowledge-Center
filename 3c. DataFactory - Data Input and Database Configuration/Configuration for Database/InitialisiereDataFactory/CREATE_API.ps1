aad16453575e77f893ec8149c38d23db4682d3a9
MariaRuedger <maria.ruedger@saxess-software.de>
2018-12-17 10:53:39 +0100

    Erzeugen von getrennten Dateien für Tabellen und API

--- 3c. DataFactory - Data Input and Database Configuration/Configuration for Database/InitialisiereDataFactory/CREATE_API.ps1
+++ 3c. DataFactory - Data Input and Database Configuration/Configuration for Database/InitialisiereDataFactory/CREATE_API.ps1
@@ -1,4 +1,7 @@
-﻿# Creates API for database(s)
+﻿## VERSION 1.3 ##
+
+
+# Creates API for database(s)
 # The script can have empty Folders
 
     #Object for Encoding
@@ -12,15 +15,9 @@
                                     Remove-Item $strScriptName
                                    }
 
-         #Permanente Erweiterungstabellen
-    cat '.\01_Permanente Erweiterungstabellen\01_Staging\*.sql',
-        '.\01_Permanente Erweiterungstabellen\02_Param\*.sql',
-        '.\01_Permanente Erweiterungstabellen\03_Calc\*.sql',
-        '.\01_Permanente Erweiterungstabellen\04_Control\*.sql',
-        '.\01_Permanente Erweiterungstabellen\05_Result\*.sql',
-
+								   
         #CustomAPI
-        '.\02_CustomAPI\01_Header, CleanUp\*.sql',
+    cat '.\02_CustomAPI\01_Header, CleanUp\*.sql',
         '.\02_CustomAPI\02_StandardAPI\*.sql',
         '.\02_CustomAPI\03_API-Modifikationen\*.sql',
         '.\02_CustomAPI\04_Staging\*.sql',
@@ -36,5 +33,33 @@
     [System.IO.File]::WriteAllLines($path, (Get-Content $path),$Utf8NoBomEncoding)
 
 
+# Creates custom tables for database(s)
+# The script can have empty Folders
 
+    #Object for Encoding
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+
+    #File Name
+    $strScriptName = "Tabellen_DataFactory.sql"
+
+    #Deletion old file and creation new file
+    If (Test-Path $strScriptName) {
+                                    Remove-Item $strScriptName
+                                   }
+
+								   
+         #Permanente Erweiterungstabellen
+    cat '.\02_CustomAPI\01_Header, CleanUp\01_Header for API script.sql',
+		'.\01_Permanente Erweiterungstabellen\01_Staging\*.sql',
+        '.\01_Permanente Erweiterungstabellen\02_Param\*.sql',
+        '.\01_Permanente Erweiterungstabellen\03_Calc\*.sql',
+        '.\01_Permanente Erweiterungstabellen\04_Control\*.sql',
+        '.\01_Permanente Erweiterungstabellen\05_Result\*.sql'        > $strScriptName -Encoding "UTF8"
+
+		
+    #Really UTF-8 without BOM
+    $path = $strScriptName
+    [System.IO.File]::WriteAllLines($path, (Get-Content $path),$Utf8NoBomEncoding)
+
+	
 
